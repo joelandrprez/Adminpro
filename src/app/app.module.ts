c02e83c8c7e7367ef59098cf3e913ccb5739da38
Joel Perez <joanpeq@gmail.com>
2021-09-06 00:45:02 -0500

    se termino de configurar las rutas hijas -04

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,37 +7,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { RouterModule, Routes } from '@angular/router'
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { BreadcrumbsComponent } from './share/breadcrumbs/breadcrumbs.component';
-import { SidebarComponent } from './share/sidebar/sidebar.component';
-import { HeaderComponent } from './share/header/header.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { PagesComponent } from './pages/pages.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
+import { PagesModule } from './pages/pages.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    BreadcrumbsComponent,
-    SidebarComponent,
-    HeaderComponent,
-    ProgressComponent,
-    Grafica1Component,
-    PagesComponent
+    NopagefoundComponent
+    
+
+
+
   ],
   imports: [
     RouterModule,//PARA QUE SE PUEDA USAAR EL ROUTER-OULET
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PagesModule, // jala las rutas de las paginas
     
   ],
   providers: [],
---
+++ src/app/share/share.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
+import { PagesComponent } from '../pages/pages.component';
+
+
+
+@NgModule({
+  declarations: [
+    BreadcrumbsComponent,
+    SidebarComponent,
+    HeaderComponent
+  ],
+  exports:[
+    BreadcrumbsComponent,
+    SidebarComponent,
+    HeaderComponent,
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ShareModule { }
